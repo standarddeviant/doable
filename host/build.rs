0f5c25e1c9dc5cae3b3cd5669b51384eb8bd52ca
standarddeviant <dave.crist@gmail.com>
2024-03-01 16:04:21 -0500

    Using .proto from other folder

--- host/build.rs
+++ host/build.rs
@@ -1,7 +1,12 @@
 // use std::io::Result;
+use std::path::Path;
 
 fn main() {
     // -> Result<()> {
-    prost_build::compile_protos(&["src/items.proto"], &["src/"]).unwrap();
+    prost_build::compile_protos(
+        &[Path::new("..").join("proto").join("items.proto")],
+        &[Path::new("..").join("proto")],
+    )
+    .unwrap();
     // Ok(())
 }
